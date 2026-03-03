d088a508223544e0a9797abcd8428355895ccb35
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-03 17:01:35 -0500

    edited useFetch hook

--- sba/src/hooks/useFetch.js
+++ sba/src/hooks/useFetch.js
@@ -1,30 +1,35 @@
-import { useState, useEfect } from 'react';
+import { useState, useEffect } from "react";
 
 const useFetch = (url) => {
-    const [data, setData] = useState();
-    const [error, setError] = useState();
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
     const fetchData = async () => {
-        useEffect(() => {
-        if (!url) return;
+      if (!url) return;
 
-        const controller = new AbortController();
+      const controller = new AbortController();
 
-        try {
-            const response = await fetch(url, { signal: controller.signal});
+      try {
+        const response = await fetch(url, { signal: controller.signal });
 
-            if (!response.ok) throw new Error('response not ok');
-            const result = await response.json();
+        if (!response.ok) throw new Error("response not ok");
+        const result = await response.json();
 
-            setData(result);
-            setError(null);
+        setData(result);
+        setError(null);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        } catch (error) {
-            setError(error);
-        } finally {
-            
-        }
-    }
+    fetchData();
+  }, [url]);
 
-        })
-}
+  return { data, loading, error };
+};
+
+export default useFetch;
--- sba/src/hooks/useLocalStorage.js
+++ sba/src/hooks/useLocalStorage.js
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
 
 const useLocalStorage = () => {
-    const [saveRecipes, setSaveRecipes] = useState();
-    
+    const [saveRecipes, setSaveRecipes] = useState(() => {
+        const item = localStorage.getItem('fav-recipes');
+       
+    });
+
 };
 
 export default useLocalStorage;
