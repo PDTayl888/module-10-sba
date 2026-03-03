9813ab85d862ead68139a0a985f3a960a818c284
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-02 23:11:03 -0500

    edited useFetch hook

--- sba/src/hooks/useFetch.js
+++ sba/src/hooks/useFetch.js
@@ -4,15 +4,27 @@ const useFetch = (url) => {
     const [data, setData] = useState();
     const [error, setError] = useState();
 
-    const fetchData = async () = {
+    const fetchData = async () => {
+        useEffect(() => {
+        if (!url) return;
+
         const controller = new AbortController();
-        
+
         try {
+            const response = await fetch(url, { signal: controller.signal});
 
-        } catch () {
+            if (!response.ok) throw new Error('response not ok');
+            const result = await response.json();
 
+            setData(result);
+            setError(null);
+
+        } catch (error) {
+            setError(error);
         } finally {
-
+            
         }
     }
+
+        })
 }
