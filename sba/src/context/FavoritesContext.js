3493afe03a3215a71650d456c7c62e0d4af37b04
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-03 19:09:10 -0500

    edited useLocalStorage

--- sba/src/context/FavoritesContext.js
+++ sba/src/context/FavoritesContext.js
@@ -1,7 +1,9 @@
 import { createContext, useContext } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 const FavoritesContext = createContext();
 
 export const FavoritesProvider = () => {
-    
+    const [favorite, setFavorites] = useLocalStorage('')
+
 }
--- sba/src/hooks/useLocalStorage.js
+++ sba/src/hooks/useLocalStorage.js
@@ -1,11 +1,18 @@
 import { useState, useEffect } from "react";
 
-const useLocalStorage = () => {
+const useLocalStorage = (recipes) => {
     const [saveRecipes, setSaveRecipes] = useState(() => {
         const item = localStorage.getItem('fav-recipes');
-       
+        return item ? JSON.parse(item) : recipes;
     });
 
+
+
+useEffect(() => {
+    localStorage.setItem('fav-recipes', saveRecipes)
+}, [saveRecipes]);
+
+    return saveRecipes;
 };
 
 export default useLocalStorage;
